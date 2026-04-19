23f05fca9302938a3ff0146f699999fbe5239ebf
Arthur Felipe <arthur.f.campos@ufv.br>
2026-04-18 23:23:09 -0300

    funcao completa do montador

--- montador.py
+++ montador.py
@@ -36,3 +36,101 @@ def imediato_para_binario(imediato, bits=12):
 
     binario = format(numero & ((1 << bits)-1), f'0{bits}b')
     return binario
+
+
+#função principal de "montagem" dos binários finais
+def montador(instrucao):
+    instrucao = instrucao.replace(',', ' ').replace('(', ' ').replace(')', ' ')
+    partes = instrucao.split()
+
+    if not partes:
+        return 'Erro: Formato inválido ou instrução não fornecida'
+    
+
+    tipo_instrucao = partes[0]
+
+    #abaixo vamos mapear pseudo-instruções
+    if tipo_instrucao == 'mv':
+        tipo_instrucao = 'add'
+
+        partes = ['add', partes[1], partes[2], 'x0']
+
+    elif tipo_instrucao == 'li':
+        tipo_instrucao = 'ori'
+
+        partes = ['ori', partes[1], 'x0', partes[2]]
+
+    elif tipo_instrucao == 'bnez':
+        tipo_instrucao = 'bne'
+
+        partes = ['bne', partes[1], 'x0', partes[2]]
+
+
+    #fazemos a identificação dos formatos de instrução e a montagem dos binários finais
+    if tipo_instrucao not in mapa_instrucoes:
+        return 'Instrução não mapeada ou não suportada'
+    
+    
+    dados = mapa_instrucoes[tipo_instrucao]
+
+    if dados['tipo'] == 'R':
+        funct3 = dados['funct3']
+        funct7 = dados['funct7']
+        opcode = dados['opcode']
+
+        rd = registrador_para_binario(partes[1])
+        rs1 = registrador_para_binario(partes[2])
+        rs2 = registrador_para_binario(partes[3])
+
+        binario_final = f'{funct7}{rs2}{rs1}{funct3}{rd}{opcode}'
+
+
+    if dados['tipo'] == 'S':
+        funct3 = dados['funct3']
+        opcode = dados['opcode']
+
+        rs2 = registrador_para_binario(partes[1])
+        
+        imm_bin = imediato_para_binario(partes[2], 12) 
+        
+        rs1 = registrador_para_binario(partes[3])
+
+        imm_1 = imm_bin[0:7] 
+        imm_2 = imm_bin[7:12]
+
+        binario_final = f'{imm_1}{rs2}{rs1}{funct3}{imm_2}{opcode}'
+
+
+    if dados['tipo'] == 'I':
+        funct3 = dados['funct3']
+        opcode = dados['opcode']
+        
+        rd = registrador_para_binario(partes[1])
+        
+        if tipo_instrucao in ['lb', 'lh', 'lw']:
+            imm_bin = imediato_para_binario(partes[2])
+            rs1 = registrador_para_binario(partes[3])
+        else:
+            rs1 = registrador_para_binario(partes[2])
+            imm_bin = imediato_para_binario(partes[3])
+        
+        binario_final = f'{imm_bin}{rs1}{funct3}{rd}{opcode}'
+
+
+    if dados['tipo'] == 'B':
+        funct3 = dados['funct3']
+        opcode = dados['opcode']
+        
+        rs1 = registrador_para_binario(partes[1])
+        rs2 = registrador_para_binario(partes[2])
+        
+        imm_bin = imediato_para_binario(partes[3], 13)
+        
+        imm_1 = imm_bin[0]
+        imm_2 = imm_bin[2:8]
+        imm_3 = imm_bin[8:12]
+        imm_4 = imm_bin[1]
+        
+        binario_final = f'{imm_1}{imm_2}{rs2}{rs1}{funct3}{imm_3}{imm_4}{opcode}'
+
+    return binario_final
